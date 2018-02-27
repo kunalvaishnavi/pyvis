761d8d80ab17f0cf64f73c44a45cf81b4b73b2df
kunalcsc630 <32287094+kunalcsc630@users.noreply.github.com>
2018-02-27 18:13:24 -0500

    Update strike_zone.py

--- pyvis/strike_zone.py
+++ pyvis/strike_zone.py
@@ -1,31 +1,35 @@
-import breakdown # breakdown.py
-import matplotlib.patches as patches
+import data # data.py
+
+# SZ in function names = strike zone (ex: batterSZ = batter strike zone)
 
 # Plots strike zone of pitches for a batter
-# Time can be a day or a range of dates (ex: batter_strike_zone('2016-05-01', 'david', 'ortiz') 
-# or batter_strike_zone('2016-04-01 to 2016-05-01', 'david', 'ortiz'))
-def batter_strike_zone(time, firstname, lastname):
-    data = process_data(time, firstname, lastname, 'batter')
-    general_strike_zone(data, firstname, lastname, 'batter')
+# Time can be a day or a range of dates (ex: batterSZ('2016-05-01', 'david', 'ortiz') 
+# or batterSZ('2016-04-01 to 2016-05-01', 'david', 'ortiz'))
+def batterSZ(time, firstname, lastname):
+    data = processData(time, firstname, lastname, 'batter')
+    if data.size == 0: return "Error: Did not find any data. Try again."
+    generalSZ(data, firstname, lastname, 'batter')
 
 # Plots strike zone of pitches for a pitcher
-# Time can be a day or a range of dates (ex: pitcher_strike_zone('2017-05-30', 'chris', 'sale') 
-# or pitcher_strike_zone('2017-05-01 to 2017-06-01', 'chris', 'sale'))
-def pitcher_strike_zone(time, firstname, lastname):
-    data = process_data(time, firstname, lastname, 'pitcher')
-    general_strike_zone(data, firstname, lastname, 'pitcher')
+# Time can be a day or a range of dates (ex: pitcherSZ('2017-05-30', 'chris', 'sale') 
+# or pitcherSZ('2017-05-01 to 2017-06-01', 'chris', 'sale'))
+def pitcherSZ(time, firstname, lastname):
+    data = processData(time, firstname, lastname, 'pitcher')
+    if data.size == 0: return "Error: Did not find any data. Try again."
+    generalSZ(data, firstname, lastname, 'pitcher')
 
 # Plots strike zone of pitches for a specific batter vs. pitcher matchup from the umpire's perspective
-# Time can be a day or a range of dates (ex: specific_matchup_strike_zone("2016-08-01", "mookie", "betts", "james", "paxton")
-# or specific_matchup_strike_zone("2016-08-01 to 2016-08-02", "mookie", "betts", "james", "paxton")
-def specific_matchup_strike_zone(time, batter_firstname, batter_lastname, pitcher_firstname, pitcher_lastname):
-    data = process_data(time, batter_firstname, batter_lastname, 'batter')
+# Time can be a day or a range of dates (ex: matchupSZ("2016-08-01", "mookie", "betts", "james", "paxton")
+# or matchupSZ("2016-08-01 to 2016-08-02", "mookie", "betts", "james", "paxton")
+def matchupSZ(time, batter_firstname, batter_lastname, pitcher_firstname, pitcher_lastname):
+    data = processData(time, batter_firstname, batter_lastname, 'batter')
+    if data.size == 0: return "Error: Did not find any data. Try again."
     pitcherid = playerid_lookup(pitcher_lastname, pitcher_firstname)
     data = data[data['pitcher'] == pitcherid['key_mlbam'][0]]
-    general_strike_zone(data, batter_firstname, batter_lastname, 'batter')
+    generalSZ(data, batter_firstname, batter_lastname, 'batter')
 
-# Plot strike zone using given data
-def general_strike_zone(data, firstname, lastname, posid):
+# Takes in data from above functions and plots strike zone
+def generalSZ(data, firstname, lastname, posid):
     print("Finding strike zone dimensions for player.")
     top = np.round(np.mean(data['sz_top']), 3) # height from ground to top of strike zone
     bot = np.round(np.mean(data['sz_bot']), 3) # height from ground to bottom of strike zone
@@ -38,7 +42,7 @@ def general_strike_zone(data, firstname, lastname, posid):
     if posid == 'batter': ax.scatter(data['plate_x'], data['plate_z'], c=setColors(data), marker='o')
     # default is x coords in batter's perspective, so multiply by -1 to reverse direction for pitcher
     elif posid == 'pitcher': ax.scatter([x_pos*-1 for x_pos in data['plate_x']], data['plate_z'], c=setColors(data), marker='o')
-    # personalize strike zone per player using https://baseballwithr.files.wordpress.com/2015/02/plate_dims.jpg
+    # personalize strike zone per player
     ax.add_patch(patches.Rectangle((-20.14/24,bot), 20.14/12, top-bot, fill=False))
     ax.set_xlim(-2,2)
     ax.set_ylim(0,6)
@@ -60,7 +64,7 @@ def checkUser(data):
             if option == "hit into play": exclude.append("hit")
             elif option == "called strikes": exclude.append("called_strike")
             elif option == "swinging strikes & fouls": exclude.extend(["swinging_strike", "foul"])
-            else: exclude.append(option[0:len(option)-1])
+            elif option == "balls": exclude.append(option[0:len(option)-1])
     return exclude
 
 # Remove unwanted data based on user's input
@@ -76,17 +80,18 @@ def fixData(data, exclude):
 
 # Set labels for graph
 def setLabels(exclude):
-    if "foul" in exclude: exclude.pop(exclude.index("foul")) # helps later on
+    if "foul" in exclude: exclude.pop(exclude.index("foul"))
     blue = patches.Patch(color='blue', label='Hit Into Play')
     red = patches.Patch(color='red', label='Called Strike')
     brown = patches.Patch(color='brown', label='Foul/Swinging Strike')
     green = patches.Patch(color='green', label='Ball')
     colors = [blue, red, brown, green]
-    # when creating labels, exception for "Foul/Swinging Strike" because of slash:
-    labels = [patch.get_label().lower().replace(" ", "_") if i != 2 else patch.get_label().lower()[5:].replace(" ", "_") for i, patch in enumerate(colors)]
-    # remove unwanted labels
+    labels = [patch.get_label() for patch in colors]
     for elm in exclude:
-        colors.pop(labels.index(elm))
+        if elm == "hit": colors.pop(labels.index("Hit Into Play"))
+        elif elm == "called_strike": colors.pop(labels.index("Called Strike"))
+        elif elm == "swinging_strike": colors.pop(labels.index("Foul/Swinging Strike"))
+        elif elm == "ball": colors.pop(labels.index("Ball"))
     return colors
 
 # Set color depending on type of outcome on pitch
