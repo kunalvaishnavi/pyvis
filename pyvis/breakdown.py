3485acfc6bf7264fbb2345c176ee56452998fa0f
kunalcsc630 <32287094+kunalcsc630@users.noreply.github.com>
2018-02-28 09:36:50 -0500

    Update breakdown.py

--- pyvis/breakdown.py
+++ pyvis/breakdown.py
@@ -7,7 +7,8 @@ import data # data.py
 # or batterBR('2016-05-01 to 2017-05-01', 'dustin', 'pedroia'))
 def batterBR(time, firstname, lastname):
     data = processData(time, firstname, lastname, 'batter')
-    if data.size == 0: return "Error: Did not find any data. Try again."
+    if data.size == 0: 
+        return "Error: Did not find any data. Try again."
     generalBR(data, 'hit event')
     
 # Pitcher event breakdown in pie chart
@@ -15,7 +16,8 @@ def batterBR(time, firstname, lastname):
 # or pitcherBR('2016-01-01 to 2017-01-01', 'chris', 'sale'))
 def pitcherBR(time, firstname, lastname):
     data = processData(time, firstname, lastname, 'pitcher')
-    if data.size == 0: return "Error: Did not find any data. Try again."
+    if data.size == 0: 
+        return "Error: Did not find any data. Try again."
     generalBR(data, 'pitch event')
     
 # Pitch selection breakdown in pie chart
@@ -23,10 +25,12 @@ def pitcherBR(time, firstname, lastname):
 # or pitchSelectionBR('2016-01-01 to 2017-01-01', 'chris', 'sale'))
 def pitchSelectionBR(time, firstname, lastname):
     data = processData(time, firstname, lastname, 'pitcher')
-    if data.size == 0: return "Error: Did not find any data. Try again."
+    if data.size == 0: 
+        return "Error: Did not find any data. Try again."
     freq = data['pitch_type'].value_counts()
     print("Frequency of pitch breakdown:")
     print(freq)
+    
     # Add to dict below as needed
     names = {'FF': '4 Seam Fastball', 'FA': '4 Seam Fastball', 'FT': '2 Seam Fastball', 'FC': 'Cutter', 
              'FS': 'Splitter', 'FO': 'Forkball', 'SI': 'Sinker', 'SL': 'Slider', 'CU': 'Curveball', 
@@ -40,6 +44,7 @@ def pitchSelectionBR(time, firstname, lastname):
             for i, r in enumerate(freq_edited):
                 if r[0] == names[elm1]:
                     freq_edited[i][1] += elm2
+                    
     print("Pie chart of pitch selection:")
     plt.pie([row[1] for row in freq_edited], labels=[row[0] for row in freq_edited], autopct='%1.1f%%')
     plt.axis('equal')
@@ -52,11 +57,13 @@ def generalBR(data, name):
     freq = data['events'].value_counts()
     print("Frequency of " + name + " breakdown:")
     print(freq)
+    
     print("Dropping infrequent events for pie chart.")
     total = sum(freq.tolist())
     freq_edited = [[elm1, elm2] for elm1, elm2 in zip(freq.keys().tolist(), freq.tolist()) if elm2 >= 0.02*total]
     other_total = [elm for elm in freq.tolist() if elm < 0.02*total]
     freq_edited.append(['other', sum(other_total)])
+    
     print("Pie chart of " + name + " breakdown:")
     plt.pie([row[1] for row in freq_edited], labels=[row[0] for row in freq_edited], autopct='%1.1f%%')
     plt.axis('equal')
